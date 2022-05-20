9c3d56a5b844ca496b4742f7a9a0cb4731d02ba1
Boguszz <87972200+Boguszz@users.noreply.github.com>
2022-05-20 14:02:26 +0000

    Deploying to gh-pages from @ Boguszz/typescript-hw-08-phonebook@0dc58e7b32efe94aac0f33561da0ef56910e31a3 🚀

---
+++ static/js/846.0c6d0b32.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktypescript_hw_08_phonebook=self.webpackChunktypescript_hw_08_phonebook||[]).push([[846],{846:function(e,t,o){o.r(t);var n=o(597),c=o(9815),p=o(184);t.default=function(){return(0,p.jsx)(c.Z,{children:(0,p.jsx)("img",{src:n,alt:"Phone book"})})}},597:function(e,t,o){e.exports=o.p+"static/media/homeBg.97001d78ceb2f1613f86.jpg"}}]);
+//# sourceMappingURL=846.0c6d0b32.chunk.js.map
